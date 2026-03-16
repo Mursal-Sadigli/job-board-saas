c276b868a91da37f34634bdf2ddc19f6a419acde
Mursal-Sadigli <sadiqli2024@gmail.com>
2026-03-16 21:50:40 +0400

    all

--- backend/server.ts
+++ backend/server.ts
@@ -27,7 +27,8 @@ app.use(helmet());
 app.use(cors({
   origin: [
     process.env.FRONTEND_URL || 'http://localhost:3000',
-    'https://nextgen-jobboard.vercel.app',
+    'https://nextgen-jobboard.vercel.app', // Eski host
+    'https://job-board-saas.vercel.app', // YENİ VERCEL HOST
     'http://localhost:3000'
   ],
   credentials: true
