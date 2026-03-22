16409c44fd8de5e64ec4e268340682c0aa28753a
Mursal-Sadigli <sadiqli2024@gmail.com>
2026-03-22 23:43:59 +0400

    hi, hello

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,8 @@
 import type { NextConfig } from "next";
-const withPWA = require("next-pwa")({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
-  turbopack: {},
+  // turbopack: {}, // Enable if needed, but keeping it simple for now
   images: {
     remotePatterns: [
       {
@@ -26,4 +20,4 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
